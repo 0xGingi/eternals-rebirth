ea2a835d16f74a92d822a6cb972f929ca50bf4f5
0xGingi <0xgingi@0xgingi.com>
2025-07-15 07:37:07 -0400

    register slash commands globally

--- src/utils/commandRegistry.ts
+++ src/utils/commandRegistry.ts
@@ -27,7 +27,7 @@ export async function registerCommands(bot: Bot) {
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
     const data = await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
+      Routes.applicationCommands(process.env.CLIENT_ID!),
       { body: commands },
     );
 
